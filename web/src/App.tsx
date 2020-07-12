b3b102a55509637a6574bf9fd9f425520c67442f
Cintia Zago <cintiazago@ivendas.com.br>
2020-07-12 18:07:36 -0300

    Creating the register page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignIn from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
