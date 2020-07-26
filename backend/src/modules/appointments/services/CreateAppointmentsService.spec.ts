b15cbcdf85b41957d910097fbe8619fa42cce263
Cintia Zago <cintiazago@gmail.com>
2020-07-26 18:22:04 -0300

    Testing create appintment, create users and test coverage configuration

--- backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentsService from './CreateAppointmentsService';
 
@@ -17,7 +19,24 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('121212121');
     });
 
-    // it('should not be able to create two appointments on the same time', () => {
-    //     expect(1 + 2).toBe(3);
-    // });
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentsService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 7, 25, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '121212121',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '121212121',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
---
+++ backend/src/modules/users/services/CreateUsersService.spec.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUsersService from './CreateUserService';
+
+describe('CreateUser', () => {
+    it('should be able to create a new user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const createUser = new CreateUsersService(fakeUsersRepository);
+
+        const user = await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        expect(user).toHaveProperty('id');
+    });
+
+    it('should not be able to create a new user with same email from another user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const createUser = new CreateUsersService(fakeUsersRepository);
+
+        await createUser.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        expect(
+            createUser.execute({
+                name: 'John Doe',
+                email: 'johndoe@example.com',
+                password: '123456',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
