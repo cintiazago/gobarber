93756770c394a98b2801ba831719490ef0d979c6
Cintia Zago <cintiazago@gmail.com>
2020-07-26 16:23:47 -0300

    Implementing controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,33 +1,13 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppoitnmentService from '@modules/appointments/services/CreateAppointmentService';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appoitmentsRouter = Router();
+const appointmentsController = new AppointmentsController();
 
 appoitmentsRouter.use(ensureAuthenticated);
 
-// appoitmentsRouter.get('/', async (request, response) => {
-//     const appointments = await appointmentsRepository.find();
-
-//     return response.json(appointments);
-// });
-
-appoitmentsRouter.post('/', async (request, response) => {
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = container.resolve(CreateAppoitnmentService);
-
-    const appointment = await createAppointment.execute({
-        date: parsedDate,
-        provider_id,
-    });
-
-    return response.json(appointment);
-});
+appoitmentsRouter.post('/', appointmentsController.create);
 
 export default appoitmentsRouter;
--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,48 +1,24 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from 'tsyringe';
 
 import uploadConfig from '@config/upload';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-import UpdateuserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 import enruseAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const userAvatarController = new UserAvatarController();
+const usersController = new UsersController();
 
-usersRouter.post('/', async (request, response) => {
-    const { name, email, password } = request.body;
-
-    const createUser = container.resolve(CreateUserService);
-
-    const user = await createUser.execute({
-        name,
-        email,
-        password,
-    });
-
-    delete user.password;
-
-    return response.json(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     enruseAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateuserAvatarService);
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatar_filename: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    },
+    userAvatarController.update,
 );
 
 export default usersRouter;
