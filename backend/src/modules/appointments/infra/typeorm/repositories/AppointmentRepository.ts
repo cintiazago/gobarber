802f982f2c5ec5d478054be990ceec5dd2f0f8d5
Cintia Zago <cintiazago@gmail.com>
2020-08-15 17:32:33 -0300

    Implementing and filtering day and month schedule from providers

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -4,6 +4,7 @@ import IAppointmentsRepository from '@modules/appointments/repositories/IAppoint
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 import Appointment from '../entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
@@ -33,6 +34,27 @@ class AppointmentsRepository implements IAppointmentsRepository {
                 date: Raw(
                     dateFieldName =>
                         `to_char(${dateFieldName}, 'MM-YYYY') = '${parsedMonth}-${year}'`,
+                ),
+            },
+        });
+        return appointments;
+    }
+
+    public async findAllInDayromProvider({
+        provider_id,
+        day,
+        month,
+        year,
+    }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+        const parsedMonth = String(month).padStart(2, '0');
+        const parsedDay = String(day).padStart(2, '0');
+
+        const appointments = await this.ormRepository.find({
+            where: {
+                provider_id,
+                date: Raw(
+                    dateFieldName =>
+                        `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
                 ),
             },
         });
--- backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -27,7 +27,47 @@ describe('ListProviderMonthAvailability', () => {
 
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
+            date: new Date(2020, 4, 20, 9, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
             date: new Date(2020, 4, 20, 10, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 11, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 12, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 13, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 14, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 17, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 17, 0, 0),
         });
 
         await fakeAppointmentsRepository.create({
@@ -45,7 +85,7 @@ describe('ListProviderMonthAvailability', () => {
             expect.arrayContaining([
                 { day: 19, available: true },
                 { day: 20, available: false },
-                { day: 21, available: false },
+                { day: 21, available: true },
                 { day: 22, available: true },
             ]),
         );
