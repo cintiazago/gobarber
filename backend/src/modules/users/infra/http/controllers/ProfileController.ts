d78a66d80f8ea12fff1e34162b9155ea2b8b2f47
Cintia Zago <cintiazago@gmail.com>
2020-08-16 16:11:46 -0300

    Using class-tranformer to change objects

--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -34,8 +35,6 @@ export default class ProfileController {
             password,
         });
 
-        delete user.password;
-
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
