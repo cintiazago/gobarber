5ee342f056ee66a11cf7e74e1164be5ead1c6913
Cintia Zago <cintiazago@gmail.com>
2020-08-16 12:04:22 -0300

    Implementing provider schedule route

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appoitmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appoitmentsRouter.use(ensureAuthenticated);
 
 appoitmentsRouter.post('/', appointmentsController.create);
+appoitmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appoitmentsRouter;
