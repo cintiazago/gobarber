8acbd955a349739764b89b0642878eb37dda541d
Cintia Zago <cintiazago@gmail.com>
2020-08-16 18:16:36 -0300

    Configuring SES AWS email provider

--- backend/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -1,31 +1,26 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import { injectable, inject } from 'tsyringe';
+import aws from 'aws-sdk';
+
+import mailConfig from '@config/mail';
 
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 import IMailProvider from '../models/IMailProvider';
 import ISendMailDTO from '../dtos/ISendMailDTO';
 
 @injectable()
-export default class EtherealMailProvider implements IMailProvider {
+export default class SESMailProvider implements IMailProvider {
     private client: Transporter;
 
     constructor(
         @inject('MailTemplateProvider')
         private mailTemplateProvider: IMailTemplateProvider,
     ) {
-        console.log('adsdfs');
-        nodemailer.createTestAccount().then(account => {
-            const transporter = nodemailer.createTransport({
-                host: account.smtp.host,
-                port: account.smtp.port,
-                secure: account.smtp.secure,
-                auth: {
-                    user: account.user,
-                    pass: account.pass,
-                },
-            });
-
-            this.client = transporter;
+        this.client = nodemailer.createTransport({
+            SES: new aws.SES({
+                apiVersion: '2010-12-01',
+                region: 'us-east-1',
+            }),
         });
     }
 
@@ -35,10 +30,12 @@ export default class EtherealMailProvider implements IMailProvider {
         subject,
         templateData,
     }: ISendMailDTO): Promise<void> {
-        const message = await this.client.sendMail({
+        const { name, email } = mailConfig.defaults.from;
+
+        await this.client.sendMail({
             from: {
-                name: from?.name || 'Equipe GoBarber',
-                address: from?.email || '<equipe@gobarber.com>',
+                name: from?.name || name,
+                address: from?.email || email,
             },
             to: {
                 name: to.name,
@@ -47,8 +44,5 @@ export default class EtherealMailProvider implements IMailProvider {
             subject,
             html: await this.mailTemplateProvider.parse(templateData),
         });
-
-        console.log('Message sent: %s', message.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
     }
 }
