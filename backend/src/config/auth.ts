18511ab34e81a29db7103646cd31399a0ca40eb7
Cintia Zago <cintiazago@gmail.com>
2020-08-16 15:51:12 -0300

    Configuring environment variables

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
     jwt: {
-        secret: '8c66bde6c33c31437c45a26c0476626f',
+        secret: process.env.APP_SECRET,
         expiresIn: '1d',
     },
 };
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
+import 'dotenv/config';
+
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import { errors } from 'celebrate';
