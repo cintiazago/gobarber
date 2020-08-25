1844c37c120150862acfef1602f2f178eddc96b1
Cintia Zago <cintiazago@gmail.com>
2020-08-25 20:11:33 -0300

    Implementing user profile update

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -6,9 +6,11 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
-import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
+
+import Profile from '../pages/Profile';
+import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
@@ -17,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
 
+    <Route path="/profile" component={Profile} isPrivate />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
